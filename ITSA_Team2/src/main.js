d851d053f1212fe348014d4e050cc8cb9cf0226c
Wynblu <goh.godewynn@gmail.com>
2025-04-10 13:16:40 +0800

    changes to ACCP ACA

--- ITSA_Team2/src/main.js
+++ ITSA_Team2/src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import countries from 'i18n-iso-countries'
+
+// Import the language data using ES Module syntax
+import enLocale from 'i18n-iso-countries/langs/en.json'
+
+// Register the locale
+countries.registerLocale(enLocale)
 
 const app = createApp(App)
+app.config.globalProperties.$countries = countries
 app.use(router)
 app.mount('#app')
--- ITSA_Team2/vite.config.js
+++ ITSA_Team2/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import commonjs from '@rollup/plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),commonjs()],
   resolve: {
     alias: [
     {
