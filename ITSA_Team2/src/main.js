d6101eac52c91152f0bf7b64f69fb04193882b9a
Kyzuma <55662306+Kyzuma@users.noreply.github.com>
2025-04-11 13:29:37 +0800

    Trying to fix Deployment

--- ITSA_Team2/src/main.js
+++ ITSA_Team2/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import * as countries from 'i18n-iso-countries'
+import countries from 'i18n-iso-countries'
 import VueTelInput from 'vue-tel-input'
 import 'vue-tel-input/vue-tel-input.css'
 
@@ -13,6 +13,7 @@ import enLocale from 'i18n-iso-countries/langs/en.json'
 countries.registerLocale(enLocale)
 const app = createApp(App)
 app.config.globalProperties.$countries = countries
+
 app.use(router)
 app.use(VueTelInput)
 app.mount('#app')
