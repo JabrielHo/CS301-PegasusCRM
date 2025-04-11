eb814b49531bd9112dffe1a8ffaa5baed5b9f674
Kyzuma <55662306+Kyzuma@users.noreply.github.com>
2025-04-11 13:52:25 +0800

    idk why it works locally but not on deployment so im trying this

--- ITSA_Team2/src/main.js
+++ ITSA_Team2/src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import * as countries from 'i18n-iso-countries'
 import VueTelInput from 'vue-tel-input'
 import 'vue-tel-input/vue-tel-input.css'
-
-// Import the language data using ES Module syntax
+import countries from 'i18n-iso-countries'
 import enLocale from 'i18n-iso-countries/langs/en.json'
 
 // Register the locale
--- ITSA_Team2/vite.config.js
+++ ITSA_Team2/vite.config.js
@@ -4,13 +4,22 @@ import commonjs from '@rollup/plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),commonjs()],
+  plugins: [vue(), commonjs()],
   resolve: {
     alias: [
-    {
-      find: './runtimeConfig',
-      replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser', // ensures browser-compatible version of AWS JS SDK is used
+      },
+    ],
+  },
+  optimizeDeps: {
+    include: ['i18n-iso-countries'], // explicitly include the module in optimized deps
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/], // include all node_modules for commonjs
+      transformMixedEsModules: true, // allow mixed esmodules and commonjs
     },
-  ]
-}
+  },
 })
